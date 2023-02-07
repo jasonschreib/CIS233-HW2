91af4de411d41ff0c59030c762a72c9f8d3ae024
Jason Schreiber <jasonschreiber@jasons-mbp.myfiosgateway.com>
2023-02-06 20:47:42 -0500

    Updated hardhat.config.js and all tests passing now, hw 2 done

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
-require("@nomicfoundation/hardhat-toolbox");
+//require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-chai-matchers");
+//require('@nomiclabs/hardhat-ethers');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+      hardhat: {
+          forking: {
+              url: 'https://eth-goerli.g.alchemy.com/v2/7_5fmV6KBViG6pvb28_t8nXJ-wiqvb7P',
+              blockNumber: 8335158,
+          },
+      },
+  },
 };
